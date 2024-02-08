29515d438b9d79cdf687c5c906f45aa3fa5e9293
Lady Godiva <watson.amber.denise@gmail.com>
2024-02-07 21:54:09 -0700

    updated database file so that indexeddb and service workers are functional

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,24 +14,24 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  // console.error('putDb not implemented');
   console.log('PUT to the database');
-  const todosDb = await openDB('jate', 1);
-  const tx = todosDb.transaction('jate', 'readwrite');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ jate: content });
   const result = await request;
-  return result;
+  console.log('Data saved to the database', result);
 };
 // console.log('Data saved to the database', result);
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('getDb not implemented');
   console.log('GET from the database');
-  const todosDb = await openDB('jate', 1);
-  const tx = todosDb.transaction('jate', 'readonly');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.get(id);
+  const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
   return result;
